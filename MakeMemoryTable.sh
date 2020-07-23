f03da39588a0224a14afffc2a2cbc24bfb4b7e93
schnoog_newSL <schnoog@schnoog.eu>
2020-07-23 20:41:44 +0200

    backsave

--- MakeMemoryTable.sh
+++ MakeMemoryTable.sh
@@ -1,3 +1,5 @@
 
 mysql newscrawler -e "DROP TABLE IF EXISTS MEM_headlines;"
 mysql newscrawler -e "CREATE TABLE MEM_headlines ENGINE=MEMORY SELECT * FROM headlines;"
+mysql newscrawler -e "ALTER TABLE MEM_headlines ADD UNIQUE myuni (msgmd5(32));"
+#mysql newscrawler -e "CREATE TABLE MEM_headlines ENGINE=MEMORY like headlines";
